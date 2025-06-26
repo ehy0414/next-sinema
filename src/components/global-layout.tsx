7d9f0fc7ded0a7504007f91069084a8124711d55
ehy0414 <djagusdyd78@gmail.com>
2025-06-26 20:30:54 +0900

    design: css 수정

--- src/components/global-layout.tsx
+++ src/components/global-layout.tsx
@@ -4,11 +4,13 @@ import { ReactNode } from "react";
 export default function GlobalLayout({children,} : { children: ReactNode;}) {
 
     return(
-        <div className="bg-black max-w-[800px] min-h-[100vh] text-white px-[20px] mx-auto">
-            <header className="text-[rgb(229,9,20)] text-[20px] py-[20px]">
+        <div className="bg-black text-white ">
+            <div className="max-w-[800px] px-[20px] mx-auto min-h-[100vh]">
+                <header className="text-[rgb(229,9,20)] text-[20px] py-[20px] ">
                 <Link href={"/"}>ONEBITE CINEMA</Link>
-            </header>
-            <main>{children}</main>
+                </header>
+                <main>{children}</main>
+            </div>
         </div>
     )
 }
